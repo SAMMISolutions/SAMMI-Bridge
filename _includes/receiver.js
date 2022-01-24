b6e38a5eaf9cc1d4ee03fbc75b50bdd76ccd48e7
christinna9031 <christinna.kr@gmail.com>
2022-01-24 00:21:03 -0800

    browser global varaible

--- _includes/receiver.js
+++ _includes/receiver.js
@@ -76,6 +76,10 @@ function connecttoboard() {
       pass: nPassBox.value,
     };
     localStorage.setItem('lsParams', JSON.stringify(ls));
+
+    // set current browser as global variable
+    LB.setVariable('browser_name', browser);
+
     ConnectionStatus('toclient', 'connected', 'Connected', 'green');
     console.log('LioranBoard Authentication successsful!');
   });
@@ -134,3 +138,21 @@ function connecttoboard() {
     LBExtensionReceived(json.CommandName, json.Data);
   });
 }
+
+// Get Browser Name
+
+const browser = (function () {
+  const test = function (regexp) { return regexp.test(window.navigator.userAgent); };
+  switch (true) {
+    case test(/OBS/i): return 'OBS';
+    case test(/edg/i): return 'Microsoft Edge';
+    case test(/trident/i): return 'Microsoft Internet Explorer';
+    case test(/firefox|fxios/i): return 'Mozilla Firefox';
+    case test(/opr\//i): return 'Opera';
+    case test(/ucbrowser/i): return 'UC Browser';
+    case test(/samsungbrowser/i): return 'Samsung Browser';
+    case test(/chrome|chromium|crios/i): return 'Google Chrome';
+    case test(/safari/i): return 'Apple Safari';
+    default: return 'Other';
+  }
+}());
