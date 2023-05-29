98219676b842eb90037e3d1d36769b70943167dd
christinna9031 <christinna.kr@gmail.com>
2023-05-29 14:16:43 -0700

    ignore sample.json

--- .github/workflows/merge-json-files.js
+++ .github/workflows/merge-json-files.js
@@ -9,7 +9,7 @@ fs.readdir(directoryPath, (err, files) => {
         return console.log('Unable to scan directory: ' + err);
     } 
     files.forEach((file) => {
-        if(path.extname(file) === '.json'){
+        if(path.extname(file) === '.json' && file !== 'sample.json'){
             let rawData = fs.readFileSync(path.join(directoryPath, file));
             let jsonData = JSON.parse(rawData);
             mergedData.extensions = [...mergedData.extensions, ...jsonData.extensions];
