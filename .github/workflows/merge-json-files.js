9d0034bc9b880907982410aaa8373243765b7683
christinna9031 <christinna.kr@gmail.com>
2023-05-29 14:44:13 -0700

    minify and merge json fixes

--- .github/workflows/merge-json-files.js
+++ .github/workflows/merge-json-files.js
@@ -7,14 +7,17 @@ let mergedData = { extensions: [] };
 fs.readdir(directoryPath, (err, files) => {
     if (err) {
         return console.log('Unable to scan directory: ' + err);
-    } 
+    }
     files.forEach((file) => {
-        if(path.extname(file) === '.json' && file !== 'sample.json'){
+        if(path.extname(file) === '.json' && file !== 'sample.json' && file !== 'extensions.json'){
             let rawData = fs.readFileSync(path.join(directoryPath, file));
             let jsonData = JSON.parse(rawData);
             mergedData.extensions = [...mergedData.extensions, ...jsonData.extensions];
         }
     });
+
+    // Before writing new data, remove the existing extensions.json file
+    fs.unlinkSync(path.join(directoryPath, 'extensions.json'));
 
     fs.writeFileSync(path.join(directoryPath, 'extensions.json'), JSON.stringify(mergedData, null, 4));
 });
